7edec28919329ffa2a2bec8fa9243aac86fc7df8
parkerduff <51238203+parkerduff@users.noreply.github.com>
2022-11-06 23:45:18 -0600

    stripping out example files and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import '@/styles/dist.css';
 import React from 'react';
-import AddressBar from '@/ui/AddressBar';
 import GlobalNav from './GlobalNav';
 
 export default function RootLayout({
@@ -18,37 +17,9 @@ export default function RootLayout({
           <div className="col-start-2">
             <GlobalNav />
           </div>
-
           <div className="col-start-3 space-y-6">
-            <AddressBar />
-
             <div className="rounded-xl border border-zinc-800 bg-black p-8">
               {children}
-            </div>
-          </div>
-
-          <div className="col-start-3 col-end-4 mt-28 flex items-center justify-center">
-            <div className="text-sm text-zinc-600">
-              Created by the <b>Next.js</b>
-              {' team at '}
-              <a href="https://vercel.com">
-                <b>Vercel</b>
-              </a>
-              {'. '}
-              <a
-                className="underline decoration-dotted underline-offset-4"
-                href="https://github.com/vercel/next.js/examples/with-turbopack"
-              >
-                View the code
-              </a>
-              {' or '}
-              <a
-                className="underline decoration-dotted underline-offset-4"
-                href="https://vercel.com/templates/next.js"
-              >
-                deploy your own
-              </a>
-              {'.'}
             </div>
           </div>
         </div>
